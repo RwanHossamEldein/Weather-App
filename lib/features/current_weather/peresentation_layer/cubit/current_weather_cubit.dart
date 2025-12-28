a1c9a4344077a7ae989b8c0e879605d8bcd2a1c0
Rwan Hossam <rwanhossam0@gmail.com>
2025-12-28 18:36:33 +0200

    Add shared preferences for saved locations

--- lib/features/current_weather/peresentation_layer/cubit/current_weather_cubit.dart
+++ lib/features/current_weather/peresentation_layer/cubit/current_weather_cubit.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/features/current_weather/data_layer/model.dart';
 import 'package:weather_app/features/current_weather/data_layer/repo.dart';
 import 'package:weather_app/features/current_weather/peresentation_layer/cubit/current_weather_state.dart';
@@ -28,11 +31,12 @@ class CurrentWeatherCubit extends Cubit<CurrentWeatherState> {
   }
 
   Future<void> addSavedLocation(String city) async {
-    if (_currentMainWeather == null) return;
+    if (_currentMainWeather == null) {
+      return;
+    }
 
     try {
       final newCityWeather = await repo.getCurrentWeather(city);
-
       if (_savedLocationsList.any(
         (item) =>
             item.location.name.toLowerCase() ==
@@ -48,9 +52,38 @@ class CurrentWeatherCubit extends Cubit<CurrentWeatherState> {
             savedLocations: List.from(_savedLocationsList),
           ),
         );
+        saveCities();
       }
     } catch (e) {
       emit(CurrentWeatherError(e.toString()));
+    }
+  }
+
+  Future<void> saveCities() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<String> citiesJson = _savedLocationsList
+        .map((city) => jsonEncode(city.toJson()))
+        .toList();
+    await prefs.setStringList('savedCities', citiesJson);
+  }
+
+  Future<void> loadSavedLocations() async {
+    final prefs = await SharedPreferences.getInstance();
+    final citiesJson = prefs.getStringList('savedCities') ?? [];
+
+    _savedLocationsList.clear();
+    for (var cityStr in citiesJson) {
+      final cityMap = jsonDecode(cityStr);
+      _savedLocationsList.add(Model.fromJson(cityMap));
+    }
+
+    if (_currentMainWeather != null) {
+      emit(
+        CurrentWeatherLoaded(
+          currentWeather: _currentMainWeather!,
+          savedLocations: List.from(_savedLocationsList),
+        ),
+      );
     }
   }
 }
